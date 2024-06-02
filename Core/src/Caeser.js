df614cb1d05a22451c2799c7adb20d2af30b030a
123artCode <150064063+123artCode@users.noreply.github.com>
2024-06-02 18:32:29 +0200

    bug solving

--- Core/src/Caeser.js
+++ Core/src/Caeser.js
@@ -1,18 +1,15 @@
     const alphabet = "abcdefghijklmnopqrstuvwxyzäöüß"
-    const sonderzeichen = "1234567890?=)(/&%$§!><°^*'+#`´_-.:,;"
-
     function caeserEncrypt(key, plain){
 
         let cipher = ""
 
         for (let i=0; i<plain.length; i++){
-            if (sonderzeichen.search(plain[i].small())){
+            if (alphabet.search(plain[i].small())){
 
-            }else{
             let position = alphabet.search(plain[i].small())
             cipher += alphabet[(position + key) % alphabet.length]
-        }
       }
+    }
 
         return cipher
     }
